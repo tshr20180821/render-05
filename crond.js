5cf466ad9580cf6a189966189d7b88c8a651ef27
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-02-24 07:50:22 +0900

    Update crond.js

--- crond.js
+++ crond.js
@@ -1,3 +1,4 @@
+/*
 require('node-cron').schedule('* * * * *', function() {
   let options = {
     hostname: process.env.SERVER_NAME,
@@ -18,3 +19,31 @@ require('node-cron').schedule('* * * * *', function() {
   scheduled: true,
   timezone: 'Asia/Tokyo'
 }).start();
+*/
+
+var CronJob = require('cron').CronJob;
+var job = new CronJob(
+	'* * * * * *',
+	function() {
+    let options = {
+      hostname: process.env.SERVER_NAME,
+      port: 443,
+      // path: '/auth/crond.php',
+      path: '/index.html',
+      method: 'GET',
+      headers: {
+        'Authorization': 'Basic ' + Buffer.from(process.env.BASIC_USER + ':' + process.env.BASIC_PASSWORD).toString('base64'),
+        'User-Agent': 'node-cron ' + (new Date()).toString() + ' ' + process.pid
+      }
+    };
+    console.log(process.pid + " START " + __filename);
+    // console.error(process.env.SERVER_NAME);
+    require('https').request(options).end();
+    console.log(process.pid + " FINISH " + __filename);
+	},
+	null,
+	true,
+  timezone: 'Asia/Tokyo'
+);
+job.start();
+
