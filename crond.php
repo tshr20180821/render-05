5ca01ab1099c0eae0ac35702bda6348abd45803b
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-02-25 07:34:22 +0900

    Update crond.php

--- crond.php
+++ crond.php
@@ -5,15 +5,13 @@ $requesturi = $_SERVER['REQUEST_URI'];
 $time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
-$mu = new MyUtils();
-
-$rc = crond($mu);
+$rc = crond();
 
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
 
 exit();
 
-function crond($mu_)
+function crond()
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
     error_log($log_prefix . 'BEGIN');
