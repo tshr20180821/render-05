10375ce86801425d2f976fbd1b3c084b3e165a16
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-02-26 05:51:12 +0900

    Update crond.js

--- crond.js
+++ crond.js
@@ -25,29 +25,28 @@ import fs from 'fs';
 var CronJob = require('cron').CronJob;
 var job = new CronJob(
   '0 * * * * *',
-	function() {
-		let options = {
-			hostname: process.env.RENDER_EXTERNAL_HOSTNAME,
-			port: 443,
-			path: '/auth/crond.php',
-			// path: '/index.html',
+  function() {
+    let options = {
+      hostname: process.env.RENDER_EXTERNAL_HOSTNAME,
+      port: 443,
+      path: '/auth/crond.php',
+      // path: '/index.html',
       method: 'GET',
       headers: {
         'Authorization': 'Basic ' + Buffer.from(process.env.BASIC_USER + ':' + process.env.BASIC_PASSWORD).toString('base64'),
         'User-Agent': 'node-cron ' + process.pid + ' ' + (require('os')).hostname()
       }
     };
-		if (fs.existsSync('/tmp/NODE_STOP_FILE') == false)
-		{
-			console.log(process.pid + " START " + __filename);
-			// console.error(process.env.SERVER_NAME);
-			require('https').request(options).end();
-			console.log(process.pid + " FINISH " + __filename);
-		}
-	},
-	null,
-	true,
-	'Asia/Tokyo'
+    if (fs.existsSync('/tmp/NODE_STOP_FILE') == false)
+    {
+      console.log(process.pid + " START " + __filename);
+      // console.error(process.env.SERVER_NAME);
+      require('https').request(options).end();
+      console.log(process.pid + " FINISH " + __filename);
+    }
+  },
+  null,
+  true,
+  'Asia/Tokyo'
 );
 job.start();
-
