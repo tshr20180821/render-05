5f9667a35aee6b2a34a075b4a2b5663d413cf09b
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-02-26 00:16:24 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,8 +10,10 @@ apache2 -v
 cat /proc/version
 cat /etc/os-release
 strings /etc/localtime
-
 echo 'Processor Count : ' $(grep -c -e processor /proc/cpuinfo)
+head -n $(($(< /proc/cpuinfo wc -l) / $(grep -c -e processor /proc/cpuinfo))) /proc/cpuinfo
+
+npm audit
 
 . /etc/apache2/envvars && exec /usr/sbin/apache2 -DFOREGROUND &
 node crond.js
