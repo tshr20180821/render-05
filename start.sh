b45a3d9b0b6b7741c545d29f8ef224310f09e9b6
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-02-25 23:15:42 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,13 @@ set -x
 
 ps aux
 
+node --version
+php --version
+apache2 -v
+cat /proc/version
+cat /etc/os-release
+strings /etc/localtime
+
 echo 'Processor Count : ' $(grep -c -e processor /proc/cpuinfo)
 
 . /etc/apache2/envvars && exec /usr/sbin/apache2 -DFOREGROUND &
