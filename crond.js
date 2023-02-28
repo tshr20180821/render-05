3341bfa9c542376cb1032da92cfa5634f1659911
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-01 08:41:54 +0900

    Update crond.js

--- crond.js
+++ crond.js
@@ -22,32 +22,36 @@ require('node-cron').schedule('* * * * *', function() {
 */
 
 var CronJob = require('cron').CronJob;
-var job = new CronJob(
-  '0 * * * * *',
-  function() {
-    let options = {
-      hostname: process.env.RENDER_EXTERNAL_HOSTNAME,
-      port: 443,
-      path: '/auth/crond.php',
-      // path: '/index.html',
-      method: 'GET',
-      headers: {
-        'Authorization': 'Basic ' + Buffer.from(process.env.BASIC_USER + ':' + process.env.BASIC_PASSWORD).toString('base64'),
-        'User-Agent': 'node-cron ' + process.pid + ' ' + process.env.BUILD_DATETIME,
-        'X-Build-DateTime': process.env.BUILD_DATETIME
-      }
-    };
-    console.log(process.pid + ' START ' + __filename + ' ' + process.env.BUILD_DATETIME);
-    require('https').request(options).end();
-    
-    //require('https').request(options, (response) => {
-    //  console.log(process.pid + ' HTTP STATUS CODE : ' + response.statusCode);
-    //});
-    
-    console.log(process.pid + ' FINISH ' + __filename);
-  },
-  null,
-  true,
-  'Asia/Tokyo'
-);
+try {
+  var job = new CronJob(
+    '0 * * * * *',
+    function() {
+      let options = {
+        hostname: process.env.RENDER_EXTERNAL_HOSTNAME,
+        port: 443,
+        path: '/auth/crond.php',
+        // path: '/index.html',
+        method: 'GET',
+        headers: {
+          'Authorization': 'Basic ' + Buffer.from(process.env.BASIC_USER + ':' + process.env.BASIC_PASSWORD).toString('base64'),
+          'User-Agent': 'node-cron ' + process.pid + ' ' + process.env.BUILD_DATETIME,
+          'X-Build-DateTime': process.env.BUILD_DATETIME
+        }
+      };
+      console.log(process.pid + ' START ' + __filename + ' ' + process.env.BUILD_DATETIME);
+      require('https').request(options).end();
+
+      //require('https').request(options, (response) => {
+      //  console.log(process.pid + ' HTTP STATUS CODE : ' + response.statusCode);
+      //});
+
+      console.log(process.pid + ' FINISH ' + __filename);
+    },
+    null,
+    true,
+    'Asia/Tokyo'
+  );
 job.start();
+} catch (ex) {
+  console.log(ex.toString());
+}
