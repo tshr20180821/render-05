84b78a17fd3db9dc48e691fc178626a0ff660d8d
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-02 07:29:33 +0900

    Update crond.js

--- crond.js
+++ crond.js
@@ -1,5 +1,6 @@
-// cron sendmail
+// package : cron sendmail
 
+const log_prefix = process.env.DEPLOY_DATETIME + ' ' + process.pid + ' ';
 const CronJob = require('cron').CronJob;
 try {
   const job = new CronJob(
@@ -16,11 +17,10 @@ try {
           'X-Deploy-DateTime': process.env.DEPLOY_DATETIME
         }
       };
-      console.log(process.env.DEPLOY_DATETIME + ' ' + process.pid + ' START ' + __filename + ' ' + process.env.DEPLOY_DATETIME);
       
       try {
         require('https').request(options, (response) => {
-          console.log(process.env.DEPLOY_DATETIME + ' ' + process.pid + ' HTTP STATUS CODE : ' + response.statusCode + ' ' + options['hostname']);
+          console.log(log_prefix + 'HTTP STATUS CODE : ' + response.statusCode + ' ' + options['hostname']);
           
           if (response.statusCode != 200 && process.env.MAIL_ADDRESS != undefined) {
             const sendmail = require('sendmail')();
@@ -36,16 +36,16 @@ try {
           }  
         }).end();
       } catch (err) {
-        console.log(process.env.DEPLOY_DATETIME + ' ' + process.pid + ' ' + err.toString());
+        console.log(log_prefix + err.toString());
       }
-
-      console.log(process.env.DEPLOY_DATETIME + ' ' + process.pid + ' FINISH ' + __filename);
     },
     null,
     true,
     'Asia/Tokyo'
   );
+  console.log(log_prefix + 'START ' + __filename );
   job.start();
+  console.log(log_prefix + 'FINISH ' + __filename);
 } catch (err) {
-  console.log(process.env.DEPLOY_DATETIME + ' ' + process.pid + ' ' + err.toString());
+  console.log(log_prefix + err.toString());
 }
