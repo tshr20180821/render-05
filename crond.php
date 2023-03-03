4628a038186a23474e4fe80eef82d18a44d13e9d
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-03 13:40:22 +0900

    Update crond.php

--- crond.php
+++ crond.php
@@ -31,6 +31,10 @@ function crond()
         return;
     }
     
+    if (!file_exists('/tmp/m_cron.db')) {
+        init_sqlite();
+    }
+    
     $sql_select = <<< __HEREDOC__
 SELECT M1.schedule
       ,M1.uri
@@ -212,6 +216,67 @@ function get_pdo()
       PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt',
     );
     return new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $options);
+}
+
+function init_sqlite()
+{
+    $log_prefix = '[' . __METHOD__ . ' ' . $_ENV['DEPLOY_DATETIME'] . '] ';
+    error_log($log_prefix . 'BEGIN');
+    
+    $pdo_sqlite = new PDO('sqlite:/tmp/m_cron.db');
+    
+    $sql_create = <<< __HEREDOC__
+CREATE TABLE m_cron (
+ schedule TEXT,
+ uri TEXT,
+ method TEXT,
+ authentication TEXT,
+ headers TEXT,
+ post_data TEXT
+)
+__HEREDOC__;
+
+    $rc = $pdo_sqlite->exec($sql_create);
+    error_log($log_prefix . ' m_cron create table result : ' . $rc);
+    
+    $sql_insert = <<< __HEREDOC__
+INSERT INTO m_cron VALUES(:b_schedule, :b_uri, :b_method, :b_authentication, :b_headers, :b_post_data)
+__HEREDOC__;
+
+    $statement_insert = $pdo_sqlite->prepare($sql_insert);
+
+    $pdo = get_pdo();
+    
+    $sql_select = <<< __HEREDOC__
+SELECT M1.schedule
+      ,M1.uri
+      ,M1.method
+      ,M1.authentication
+      ,M1.headers
+      ,M1.post_data
+  FROM m_cron M1
+ WHERE M1.enable = TRUE
+ ORDER BY M1.uri
+__HEREDOC__;
+
+    $statement_select = $pdo->prepare($sql_select);
+    $rc = $statement_select->execute();
+    $results = $statement_select->fetchAll();
+    
+    foreach ($results as $row) {
+        $statement_insert->execute([
+            ':b_schedule' => $row['schedule'],
+            ':b_uri' => $row['uri'],
+            ':b_method' => $row['method'],
+            ':b_authentication' => $row['authentication'],
+            ':b_headers' => $row['headers'],
+            ':b_post_data' => $row['post_data'],
+        ]);
+        error_log($log_prefix . ' insert result : ' . $statement_insert->rowCount() . ' ' . $row['schedule'] . ' ' . $row['uri']);
+    }
+
+    $pdo = null;
+    $pdo_sqlite = null;
 }
 
 function get_contents_multi($urls_, $multi_options_ = null)
