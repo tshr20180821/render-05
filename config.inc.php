dfd23004011b51258c8eac59fe9174c541f2dc59
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-06 08:44:03 +0900

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -32,6 +32,8 @@ $cfg['Servers'][$i]['auth_type'] = 'cookie';
 $cfg['Servers'][$i]['host'] = $_ENV['DB_SERVER'];
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
+$cfg['Servers'][$i]['ssl'] = true;
+$cfg['Servers'][$i]['ssl_ca'] = '/etc/ssl/certs/ca-certificates.crt';
 
 /**
  * phpMyAdmin configuration storage settings.
