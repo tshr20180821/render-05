3232b2b3d6a822c23dcdf8173fe7d5177cf9acc6
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-06 08:21:49 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -21,6 +21,8 @@ npm list --depth=0
 php -l /var/www/html/auth/crond.php
 node -c crond.js
 
+export BLOWFISH_SECRET=$(cat /dev//urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
+
 export DEPLOY_DATETIME=$(date +'%Y%m%d%H%M%S')
 
 echo ServerName ${RENDER_EXTERNAL_HOSTNAME} >/etc/apache2/sites-enabled/server_name.conf
