67645efd3ec2dfbb7be00899ca02a6605b4411b1
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-10 08:20:29 +0900

    Update start.js

--- start.js
+++ start.js
@@ -2,24 +2,24 @@
 
 const log4js = require('log4js');
 log4js.configure({
-	"appenders": {
-		"out": {
-			"type": "stdout",
-			"layout": {
-				"type": "pattern",
-				"pattern": "%d{yyyy-MM-dd hh:mm:ss} %p %f{1} %l %m"
-			}
-		}
-	},
-	"categories": {
-		"default": {
-			"appenders": [
-				"out"
-			],
-			"level": "info",
-			"enableCallStack": true
-		}
-	}
+  "appenders": {
+    "out": {
+      "type": "stdout",
+      "layout": {
+        "type": "pattern",
+        "pattern": "%d{yyyy-MM-dd hh:mm:ss} %p %f{1} %l %m"
+      }
+    }
+  },
+  "categories": {
+    "default": {
+      "appenders": [
+        "out"
+      ],
+      "level": "info",
+      "enableCallStack": true
+    }
+  }
 });
         
 const logger = log4js.getLogger();
