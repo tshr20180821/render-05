051a5c6c440606a81d73e9105aece8fd6086f21e
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-10 09:34:48 +0900

    Update crond.js

--- crond.js
+++ crond.js
@@ -91,18 +91,13 @@ function send_mail(subject_, body_)
   };
 
   (async () => {
-    try {
-      const smtp = require('nodemailer').createTransport(smtp_options);
-      const result = await smtp.sendMail(mail, function(err, info) {
-        if (err) {
-          logger.warn(log_prefix + 'Send Mail Result : ' + err.toString());
-        } else {
-          logger.info(log_prefix + 'Send Mail Result : ' + info.message);
-        }
-      });
-      // logger.info(log_prefix + ' Send Mail Result : ' + result);
-    } catch (err) {
-      logger.warn(log_prefix + err.toString());
-    }
+    const smtp = require('nodemailer').createTransport(smtp_options);
+    const result = await smtp.sendMail(mail, function(err, info) {
+      if (err) {
+        logger.warn(log_prefix + err.toString());
+      } else {
+        logger.info(log_prefix + info.messageId);
+      }
+    });
   })();
 }
