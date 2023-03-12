46d6bacd66f20ab1f5a1881b0b22f09f516f9a87
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-13 04:11:34 +0900

    Update log.php

--- log.php
+++ log.php
@@ -14,7 +14,19 @@ class Log
     }
     
     private function output($message_, $level_, $color_) {
-        $array = debug_backtrace()[2];
+        $array = debug_backtrace();
+        array_shift($array);
+        array_shift($array);
+        if (count($array) > 0) {
+            $value = array_shift($array);
+            $file = basename($value['file']);
+            $line = $value['line'];
+            $function = $value['function'];
+        } else {
+            $file = '';
+            $line = '';
+            $function = '';
+        }
         $log_header = date('Y-m-d H:i:s.') . substr(explode(".", (microtime(true) . ""))[1], 0, 3)
             . ' ' . $_ENV['DEPLOY_DATETIME'] . ' ' . getmypid() . " ${level_} " . basename($array['file']) . ' ' . $array['line'] . ' ' . $array['function'];
         file_put_contents('php://stderr', "\033[0;${color_}m${log_header}\033[0m" . ' ' . $message_ . "\n");
