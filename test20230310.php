fa7e956ed872b3318d5cf89ae87ca1cc49e0a9b7
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-13 00:41:14 +0900

    Update test20230310.php

--- test20230310.php
+++ test20230310.php
@@ -10,6 +10,6 @@ $logger = Logger::getLogger('default');
 $logger->debug('debug_message');
 
 // file_put_contents("php://stderr", "stderr message!\n");
-file_put_contents("php://stderr", "\033[0;31mmessage\033[0m\n");
+file_put_contents("php://stderr", "\033[0;32mmessage\033[0m\n");
 
 echo 'test4';
