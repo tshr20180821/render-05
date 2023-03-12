218096e1417b2dabb4192176730862ae5d900ebf
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-13 02:05:16 +0900

    Update log.php

--- log.php
+++ log.php
@@ -6,11 +6,16 @@ class Log
     }
     
     public function info($message_) {
-        $array = end(debug_backtrace());
-        $file = $array['file'];
+        output($message_, 'INFO', '32', end(debug_backtrace()));
+    }
+    
+    public function warn($message_) {
+        output($message_, 'WARN', '33', end(debug_backtrace()));
+    }
+    
+    private function output($message_, $level_, $color_, $array_) {
         $log_header = date('Y-m-d H:i:s.') . substr(explode(".", (microtime(true) . ""))[1], 0, 3)
-            . ' ' . $_ENV['DEPLOY_DATETIME'] . ' ' . getmypid() . ' INFO ' . basename($array['file']) . ' ' . $array['line'];
-        file_put_contents('php://stderr', "\033[0;32m${log_header}\033[0m" . ' ' . $message_ . "\n");
-        file_put_contents('php://stderr', "\033[0;32mcolor test message\033[0m\n");
+            . ' ' . $_ENV['DEPLOY_DATETIME'] . ' ' . getmypid() . " ${level_} " . basename($array_['file']) . ' ' . $array_['line'];
+        file_put_contents('php://stderr', "\033[0;${color_}m${log_header}\033[0m" . ' ' . $message_ . "\n");
     }
 }
