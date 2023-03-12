60f394d93a54eaa1524888130c57e56a81576e63
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-13 06:33:04 +0900

    Update health_check.php

--- health_check.php
+++ health_check.php
@@ -22,23 +22,23 @@ $atom = <<< __HEREDOC__
 <?xml version="1.0" encoding="utf-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom">
  <title>Health Check __FQDN__</title>
- <link href="http://example.org/"/>
+ <link href="https://example.org/"/>
  <updated>2022-01-01T00:00:00Z</updated>
  <author>
-   <name>__FQDN__</name>
+   <name>Health Check</name>
  </author>
- <id>tag:__FQDN__</id>
+ <id>urn:uuid:60a76c80-d399-11d9-b93C-0003939e0af8</id>
  <entry>
    <title>Health Check __UPDATED__</title>
-   <link href="http://example.org/"/>
-   <id>tag:__ID__</id>
-   <updated>__FQDN__ __UPDATED__</updated>
-   <summary>__FQDN__ __UPDATED__</summary>
+   <link href="https://example.org/"/>
+   <id>__ID__</id>
+   <updated>__UPDATED__</updated>
+   <summary>__UPDATED__</summary>
  </entry>
 </feed>
 __HEREDOC__;
     
-    $atom = str_replace('__ID__', $_ENV['RENDER_EXTERNAL_HOSTNAME'] . '-' . uniqid(), $atom);
+    $atom = str_replace('__ID__', uniqid(), $atom);
     $atom = str_replace('__FQDN__', $_ENV['RENDER_EXTERNAL_HOSTNAME'], $atom);
     $atom = str_replace('__UPDATED__', date('Y-m-d') . 'T' . date('H:i:s') . '+09', $atom);
 
