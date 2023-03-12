98fdb9a505727cd068aad4db0c47a1d815fbd30d
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-13 01:32:53 +0900

    Update log.php

--- log.php
+++ log.php
@@ -8,9 +8,8 @@ class Log
     public function info($message_) {
         $array = end(debug_backtrace());
         $file = $array['file'];
-        
-        file_put_contents('php://stderr', "\033[0;32" . date('Y-m-d H:i:s.u') .
-                          ' ' . $_ENV['DEPLOY_DATETIME'] . ' ' . getmypid() . ' INFO ' . basename($array['file']) . ' ' . $array['line'] . "\033[0m" .
-                          ' ' . $message_ . "\n");
+        $log_header = date('Y-m-d H:i:s.') . substr(explode(".", (microtime(true) . ""))[1], 0, 3)
+            . ' ' . $_ENV['DEPLOY_DATETIME'] . ' ' . getmypid() . ' INFO ' . basename($array['file']) . ' ' . $array['line'];
+        file_put_contents('php://stderr', "\033[0;32${log_header}\033[0m" . ' ' . $message_ . "\n");
     }
 }
