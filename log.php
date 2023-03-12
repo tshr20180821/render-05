03e4b73c07d6d1e237d0dc89a2f7d5e4d2a5c015
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-13 08:56:29 +0900

    Update log.php

--- log.php
+++ log.php
@@ -20,16 +20,18 @@ class Log
             $value = array_shift($array);
             $file = basename($value['file']);
             $line = $value['line'];
-            $function = '-';
+            $function_chain = '[-]';
         } else {
             array_shift($array);
-            $value = array_shift($array);
-            $file = basename($value['file']);
-            $line = $value['line'];
-            $function = $value['function'];
+            $function_chain = '';
+            foreach (array_reverse($array) as $value) {
+                $file = basename($value['file']);
+                $line = $value['line'];
+                $function_chain .= '[' . $value['function'] . ']';
+            }
         }
         $log_header = date('Y-m-d H:i:s.') . substr(explode(".", (microtime(true) . ""))[1], 0, 3)
             . ' ' . $_ENV['DEPLOY_DATETIME'] . ' ' . trim(getmypid() . " ${level_} ${file} ${line}");
-        file_put_contents('php://stderr', "\033[0;${color_}m${log_header}\033[0m [${function}] ${message_}\n");
+        file_put_contents('php://stderr', "\033[0;${color_}m${log_header}\033[0m ${function_chain} ${message_}\n");
     }
 }
