63a3728c90e2b210acb211e394369aadaf2ac307
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-13 09:02:39 +0900

    Update log.php

--- log.php
+++ log.php
@@ -2,15 +2,19 @@
 
 class Log
 {
+    $colors = [];
+    $colors['INFO'] = '32';
+    $colors['WARN'] = '33';
+    
     function __construct() {
     }
     
     public function info($message_) {
-        $this->output($message_, 'INFO', '32');
+        $this->output($message_, 'INFO');
     }
     
     public function warn($message_) {
-        $this->output($message_, 'WARN', '33');
+        $this->output($message_, 'WARN');
     }
     
     private function output($message_, $level_, $color_) {
@@ -32,6 +36,6 @@ class Log
         }
         $log_header = date('Y-m-d H:i:s.') . substr(explode(".", (microtime(true) . ""))[1], 0, 3)
             . ' ' . $_ENV['DEPLOY_DATETIME'] . ' ' . trim(getmypid() . " ${level_} ${file} ${line}");
-        file_put_contents('php://stderr', "\033[0;${color_}m${log_header}\033[0m ${function_chain} ${message_}\n");
+        file_put_contents('php://stderr', "\033[0;" . $colors[$level_] . "m${log_header}\033[0m ${function_chain} ${message_}\n");
     }
 }
