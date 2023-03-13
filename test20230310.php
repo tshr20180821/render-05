88c4370eca462a13a53f224157e85765ab1f34e7
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-14 04:51:14 +0900

    Update test20230310.php

--- test20230310.php
+++ test20230310.php
@@ -11,4 +11,18 @@ $log->warn('message');
 $log->error('message');
 $log->fatal('message');
 
-echo 'test10';
+echo 'test11';
+
+$c = new Test20230310();
+$c->test20230310();
+
+exit();
+
+class Test20230310
+{
+    const LIST_YOBI = array('日', '月', '火', '水', '木', '金', '土');
+    
+    public function test20230310() {
+        $log->warn(LIST_YOBI[0]);
+    }
+}
