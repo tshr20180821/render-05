3045ce9c93c1d7caf05176e58801e99f11865e4f
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-14 04:44:21 +0900

    Update test20230310.php

--- test20230310.php
+++ test20230310.php
@@ -4,7 +4,11 @@ include('./log.php');
 
 $log = new Log();
 
+$log->trace('message');
+$log->debug('message');
 $log->info('message');
 $log->warn('message');
+$log->error('message');
+$log->fatal('message');
 
-echo 'test9';
+echo 'test10';
