5b1e0458d963c5af3d59b914d5367eeab520043f
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-13 10:57:12 +0900

    Update update_sqlite.php

--- update_sqlite.php
+++ update_sqlite.php
@@ -1,29 +1,45 @@
 <?php
 
-error_log('START ' . __FILE__ . ' ' . $_ENV['DEPLOY_DATETIME']);
+include('./log.php');
 
-$pdo_sqlite = new PDO('sqlite:/tmp/m_cron.db');
+$log = new Log();
 
-$sql_delete = <<< __HEREDOC__
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+$log->info("START ${requesturi}");
+
+update_sqlite($log);
+
+$log->info("FINISH " . substr((microtime(true) - $time_start), 0, 7) . 's');
+
+exit();
+
+function update_sqlite($log_)
+{
+    $log_->info('BEGIN');
+    
+    $pdo_sqlite = new PDO('sqlite:/tmp/m_cron.db');
+
+    $sql_delete = <<< __HEREDOC__
 DELETE FROM m_cron
 __HEREDOC__;
 
-$rc = $pdo_sqlite->exec($sql_delete);
-error_log('m_cron delete result : ' . $rc);
+    $rc = $pdo_sqlite->exec($sql_delete);
+    $log_->info('m_cron delete result : ' . $rc);
 
-$sql_insert = <<< __HEREDOC__
+    $sql_insert = <<< __HEREDOC__
 INSERT INTO m_cron VALUES(:b_schedule, :b_uri, :b_method, :b_authentication, :b_headers, :b_post_data)
 __HEREDOC__;
 
-$statement_insert = $pdo_sqlite->prepare($sql_insert);
+    $statement_insert = $pdo_sqlite->prepare($sql_insert);
 
-$dsn = "mysql:host={$_ENV['DB_SERVER']};dbname={$_ENV['DB_NAME']}";
-$options = array(
-    PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt',
-);
-$pdo = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $options);
+    $dsn = "mysql:host={$_ENV['DB_SERVER']};dbname={$_ENV['DB_NAME']}";
+    $options = array(
+        PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt',
+    );
+    $pdo = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $options);
 
-$sql_select = <<< __HEREDOC__
+    $sql_select = <<< __HEREDOC__
 SELECT M1.schedule
       ,M1.uri
       ,M1.method
@@ -35,23 +51,22 @@ SELECT M1.schedule
  ORDER BY M1.uri
 __HEREDOC__;
 
-$statement_select = $pdo->prepare($sql_select);
-$rc = $statement_select->execute();
-$results = $statement_select->fetchAll();
+    $statement_select = $pdo->prepare($sql_select);
+    $rc = $statement_select->execute();
+    $results = $statement_select->fetchAll();
 
-foreach ($results as $row) {
-    $statement_insert->execute([
-        ':b_schedule' => $row['schedule'],
-        ':b_uri' => $row['uri'],
-        ':b_method' => $row['method'],
-        ':b_authentication' => $row['authentication'],
-        ':b_headers' => $row['headers'],
-        ':b_post_data' => $row['post_data'],
-    ]);
-    error_log('insert result : ' . $statement_insert->rowCount() . ' ' . $row['schedule'] . ' ' . $row['uri']);
+    foreach ($results as $row) {
+        $statement_insert->execute([
+            ':b_schedule' => $row['schedule'],
+            ':b_uri' => $row['uri'],
+            ':b_method' => $row['method'],
+            ':b_authentication' => $row['authentication'],
+            ':b_headers' => $row['headers'],
+            ':b_post_data' => $row['post_data'],
+        ]);
+        $log_->info('insert result : ' . $statement_insert->rowCount() . ' ' . $row['schedule'] . ' ' . $row['uri']);
+    }
+
+    $pdo = null;
+    $pdo_sqlite = null;
 }
-
-$pdo = null;
-$pdo_sqlite = null;
-
-error_log('FINISH ' . __FILE__);
