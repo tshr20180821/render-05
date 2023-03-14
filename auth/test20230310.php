64a5f9629f89c2ab752f6ee42c7fcf1ae5f83d07
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-14 11:07:36 +0900

    Update test20230310.php

--- auth/test20230310.php
+++ auth/test20230310.php
@@ -21,7 +21,7 @@ class Test20230310
         // $log->warn(print_r(json_decode($res), true));
         
         $token = json_decode($res)->token;
-        $log->warn($token);
+        // $log->warn($token);
         
         $url = 'https://registry-1.docker.io/v2/php/tags/list';
         $ch = curl_init();
