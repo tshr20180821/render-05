82694da60a435c8331a62cd9bab6ba2e7cdb2ee5
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-14 12:02:44 +0900

    Update test20230310.php

--- auth/test20230310.php
+++ auth/test20230310.php
@@ -2,7 +2,7 @@
 
 include('./log.php');
 
-echo 'test18';
+echo 'test19';
 
 $c = new Test20230310();
 $c->test20230310();
@@ -24,9 +24,10 @@ class Test20230310
         $url = 'https://registry-1.docker.io/v2/library/php/tags/list';
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Bearer ' . $token, 'Accept: application/json',]);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_URL, $url);
         $res = curl_exec($ch);
-        $http_code = (string)curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $http_code = (string)curl_getinfo($ch, CURLINFO_HTTP_CODE); 
         curl_close($ch);
         
         $log->warn('HTTP CODE : ' . $http_code);
