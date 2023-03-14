49604590c60353f9e0a57991038c3eaeb399f049
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-14 10:39:52 +0900

    Update test20230310.php

--- auth/test20230310.php
+++ auth/test20230310.php
@@ -16,8 +16,20 @@ class Test20230310
     public function test20230310() {
         $log = new Log();
         
-        $res = file_get_contents('https://auth.docker.io/token');
+        $res = file_get_contents('https://auth.docker.io/token?service=registry.docker.io&scope=repository:php:pull');
         
-        $log->warn(print_r(json_decode($res), true));
+        // $log->warn(print_r(json_decode($res), true));
+        
+        $token = json_decode($res)['token'];
+        $log->warn($token);
+        
+        $url = 'https://registry-1.docker.io/v2/php/tags/list';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Bearer ' . $token, 'Accept: application/json',]);
+        $res = curl_exec($ch);
+        $http_code = (string)curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        
+        $log->warn($res);
     }
 }
