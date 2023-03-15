930a2a407f2b647c9df88a6501c60f3075c17494
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-03-15 10:26:05 +0900

    Update start.js

--- start.js
+++ start.js
@@ -11,44 +11,7 @@ logger.info('START');
 
 const mu = require('./MyUtils.js');
 
-mu.send_mail(process.env.RENDER_EXTERNAL_HOSTNAME + ' START ' + process.env.DEPLOY_DATETIME,
-  process.env.RENDER_EXTERNAL_HOSTNAME + ' START ' + process.env.DEPLOY_DATETIME);
+var message = process.env.RENDER_EXTERNAL_HOSTNAME + ' START ' + process.env.DEPLOY_DATETIME;
+mu.send_mail(message, message);
 
 logger.info('FINISH');
-
-/*
-function send_mail(subject_, body_)
-{
-  if (process.env.MAIL_ADDRESS == undefined) {
-    return;
-  }
-  
-  const smtp_options = {
-    host: process.env.SMTP_SERVER,
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASSWORD,
-    },
-  };
-
-  const mail = {
-    from: process.env.SMTP_USER,
-    to: process.env.MAIL_ADDRESS,
-    subject: subject_,
-    text: body_
-  };
-
-  (async () => {
-    const smtp = require('nodemailer').createTransport(smtp_options);
-    const result = await smtp.sendMail(mail, function(err, info) {
-      if (err) {
-        logger.warn(err.toString());
-      } else {
-        logger.info(info.messageId);
-      }
-    });
-  })();
-}
-*/
