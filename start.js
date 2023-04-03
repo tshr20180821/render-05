08bbf2dd4ece3e5c95c550cff878d0de16216ee7
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-04-03 13:12:25 +0900

    Update start.js

--- start.js
+++ start.js
@@ -6,6 +6,7 @@ const logger = mu.get_logger();
 logger.info('START');
 
 var message = process.env.RENDER_EXTERNAL_HOSTNAME + ' START ' + process.env.DEPLOY_DATETIME;
-mu.send_mail(message, message);
+// mu.send_mail(message, message);
+mu.send_slack_message(message);
 
 logger.info('FINISH');
