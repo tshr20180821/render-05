516728d88e3875aabe50760554aafc0edcd346ad
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-04-27 10:47:35 +0900

    Update test20230427.php

--- auth/test20230427.php
+++ auth/test20230427.php
@@ -16,9 +16,13 @@ $log->debug($res);
 
 $pdo = null;
 
+$res = preg_replace('/ENGINE=InnoDB.+/', '', $res);
 $res = preg_replace('/COLLATE \w+/', '', $res);
 $res = str_replace('DEFAULT NULL', '', $res);
 $res = str_replace('AUTO_INCREMENT', '', $res);
 $res = str_replace(' int ', ' INTEGER ', $res);
+$res = preg_replace('/varchar\(\d+\)/', 'TEXT', $res);
+$res = preg_replace('/tinyint\(\d+\)/', 'INTEGER', $res);
+$res = str_replace('`', '', $res);
 
 $log->debug($res);
