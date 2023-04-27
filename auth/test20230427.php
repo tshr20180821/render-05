ba032d98a9bcf8a9d56ceea82085b8914c798114
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-04-27 10:41:47 +0900

    Update test20230427.php

--- auth/test20230427.php
+++ auth/test20230427.php
@@ -17,8 +17,8 @@ $log->debug($res);
 $pdo = null;
 
 $res = preg_replace('/COLLATE \w+/', '', $res);
-$res = str_replace('DEFAULT NULL', ''. $res);
-$res = str_replace('AUTO_INCREMENT', ''. $res);
-$res = str_replace(' int ', ' INTEGER '. $res);
+$res = str_replace('DEFAULT NULL', '', $res);
+$res = str_replace('AUTO_INCREMENT', '', $res);
+$res = str_replace(' int ', ' INTEGER ', $res);
 
 $log->debug($res);
