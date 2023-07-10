5439ea59e29eb7dfd09ce4b56d94a7c195728095
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-07-10 15:36:53 +0900

    start.sh の更新

--- start.sh
+++ start.sh
@@ -28,7 +28,7 @@ node -c start.js
 ls -lang /var/www/html/
 
 # phpMyAdmin
-export BLOWFISH_SECRET=$(cat /dev//urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+export BLOWFISH_SECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
 export DEPLOY_DATETIME=$(date +'%Y%m%d%H%M%S')
 sed -i s/__DEPLOY_DATETIME__/${DEPLOY_DATETIME}/ /etc/apache2/sites-enabled/apache.conf
