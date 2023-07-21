a0513c9149fe7a15373d19e4f727467e817afbfc
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-07-21 09:07:58 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -50,8 +50,7 @@ curl -sS -X POST -H "Authorization: Bearer ${SLACK_TOKEN}" \
   -d "text=${VERSION}" -d "channel=${SLACK_CHANNEL_01}" https://slack.com/api/chat.postMessage >/dev/null \
 && sleep 1s \
 && curl -sS -X POST -H "Authorization: Bearer ${SLACK_TOKEN}" \
-  -d "text=${VERSION}" -d "channel=${SLACK_CHANNEL_02}" https://slack.com/api/chat.postMessage >/dev/null \
-&& sleep 1s
+  -d "text=${VERSION}" -d "channel=${SLACK_CHANNEL_02}" https://slack.com/api/chat.postMessage >/dev/null &
 
 # node start.js &
 
