c9dd32aed9fd2daec1fdd199a1bbe17e90cbfc82
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-10-01 23:04:52 +0900

    Update crond.js

--- crond.js
+++ crond.js
@@ -73,10 +73,20 @@ try {
       }
       global.gc();
       const memory_usage = process.memoryUsage();
-      logger.info('FINISH Heap Total : '
+      var message = 'FINISH Heap Total : '
                   + Math.floor(memory_usage.heapTotal / 1024).toLocaleString()
                   + 'KB Used : '
-                  + Math.floor(memory_usage.heapUsed / 1024).toLocaleString() + 'KB');
+                  + Math.floor(memory_usage.heapUsed / 1024).toLocaleString() + 'KB';
+      const request = require('https').request('https://logs-01.loggly.com/inputs/' + process.env.LOGGLY_TOKEN + '/tag/' + process.env.RENDER_EXTERNAL_HOSTNAME + '/',
+                                               {
+                                                 method: "POST",
+                                                 headers: {
+                                                   "content-type": "text/plain",
+                                                 }
+                                               });
+      request.write(message);
+      request.end();
+      logger.info(message);
     },
     null,
     true,
