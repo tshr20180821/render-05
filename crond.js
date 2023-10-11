ba629c80cef976e414e44bb6819e1aa1a02fec43
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-10-11 10:06:25 +0900

    Update crond.js

--- crond.js
+++ crond.js
@@ -44,6 +44,7 @@ try {
           if (res.statusCode != 200
               && process.env.MAIL_ADDRESS != undefined
               ) {
+            // https://process.env.RENDER_EXTERNAL_HOSTNAME/cdn-cgi/trace
             mu.send_slack_message('HTTP STATUS CODE : ' + res.statusCode + ' ' + process.env.RENDER_EXTERNAL_HOSTNAME);
           }
           
