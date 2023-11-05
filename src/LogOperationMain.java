9fb301378f65b3f9a0b3c4948fe4d53ec01b00fe
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-05 12:22:59 +0900

    Update LogOperationMain.java

--- src/LogOperationMain.java
+++ src/LogOperationMain.java
@@ -17,7 +17,7 @@ public final class LogOperationMain {
 
     public static void main(String[] args) {
         System.setProperty("java.util.logging.SimpleFormatter.format",
-                "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS.%1$tL %4$s %2$s %5$s%6$s%n");
+                "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS.%1$tL \033[33m%4$s %2$s\033[0m %5$s%6$s%n");
         Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
         logger.setLevel(Level.ALL);
         _logger = logger;
