a9274ff69df64932c29af00e9196cbeae69a5bab
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-29 11:05:35 +0900

    Update start.sh

--- app/start.sh
+++ app/start.sh
@@ -56,6 +56,7 @@ echo ${SASL_PASSWORD} | saslpasswd2 -p -a memcached -c memcached
 chown memcached:memcached /etc/sasldb2
 ls -lang /etc/sasldb2
 cat /etc/sasldb2
+sleep 10s
 # chown memcached:memcached ${MEMCACHED_SASL_PWDB}
 # sasldblistusers2
 export SASL_CONF_PATH=/tmp/memcached.conf
