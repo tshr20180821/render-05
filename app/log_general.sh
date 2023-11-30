d2b678e0ea2a731c3ad2bbf168970ac05f07a52a
tshr20180821 <62584685+tshr20180821@users.noreply.github.com>
2023-11-30 11:17:22 +0900

    Update log_general.sh

--- app/log_general.sh
+++ app/log_general.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 pid=$$
+string_part_1="${RENDER_EXTERNAL_HOSTNAME} ${DEPLOY_DATETIME} ${pid} ${1}"
+url="https://logs-01.loggly.com/inputs/${LOGGLY_TOKEN}/tag/${RENDER_EXTERNAL_HOSTNAME},${RENDER_EXTERNAL_HOSTNAME}_${DEPLOY_DATETIME},${1}/"
 
 while read line; do
   dt=$(date '+%Y-%m-%d %H:%M:%S.%3N')
-  curl -X POST -s -o /dev/null -H "Content-Type: text/plain" -d "${dt} ${RENDER_EXTERNAL_HOSTNAME} ${DEPLOY_DATETIME} ${pid} ${1} ${line}" \
-    "https://logs-01.loggly.com/inputs/${LOGGLY_TOKEN}/tag/${RENDER_EXTERNAL_HOSTNAME},${RENDER_EXTERNAL_HOSTNAME}_${DEPLOY_DATETIME},${1}/" &
+  string_part_2="${dt} ${string_part_1} ${line}"
+  # curl -X POST -s -o /dev/null -H "Content-Type: text/plain" -d "${dt} ${RENDER_EXTERNAL_HOSTNAME} ${DEPLOY_DATETIME} ${pid} ${1} ${line}" \
+  #   "https://logs-01.loggly.com/inputs/${LOGGLY_TOKEN}/tag/${RENDER_EXTERNAL_HOSTNAME},${RENDER_EXTERNAL_HOSTNAME}_${DEPLOY_DATETIME},${1}/" &
+  curl -X POST -s -o /dev/null -H "Content-Type: text/plain" -d "${string_part_2}" "${url}" &
 
-  echo "${dt} ${RENDER_EXTERNAL_HOSTNAME} ${DEPLOY_DATETIME} ${pid} ${1} ${line}"
+  echo "${string_part_2}"
 done
